8dcc44c64aaee3b86ce0cb2deb5c7dfc2a657963
gomzyakov <alexander.gomzyakov@gmail.com>
2023-11-05 14:35:35 +0500

    Fix test

--- tests/GeneratorTest.php
+++ tests/GeneratorTest.php
@@ -6,6 +6,7 @@ namespace Tests;
 
 use GoodFirstIssue\Generator;
 use GoodFirstIssue\GitHubAPIClient;
+use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -15,7 +16,7 @@ final class GeneratorTest extends TestCase
 {
     public function test_instance_of_generator(): void
     {
-        $generator = new Generator(__DIR__, new GitHubAPIClient());
+        $generator = new Generator(__DIR__, new GitHubAPIClient(new Client()));
 
         $this->assertInstanceOf(Generator::class, $generator);
     }
---
+++ tests/GitHubAPIClientTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Tests;
+
+use GoodFirstIssue\GitHubAPIClient;
+use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \GoodFirstIssue\GitHubAPIClient
+ */
+final class GitHubAPIClientTest extends TestCase
+{
+    public function test_instance_of_generator(): void
+    {
+        $generator = new GitHubAPIClient(new Client());
+
+        $this->assertInstanceOf(GitHubAPIClient::class, $generator);
+    }
+}
