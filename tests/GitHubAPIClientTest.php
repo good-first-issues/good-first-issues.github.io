ea5b174a5694af474de1ab6d47f7c2ddd7918ac6
gomzyakov <alexander.gomzyakov@gmail.com>
2024-07-27 15:24:30 +0500

    Fix tests

--- tests/GitHubAPIClientTest.php
+++ tests/GitHubAPIClientTest.php
@@ -8,9 +8,6 @@ use GoodFirstIssue\GitHubAPIClient;
 use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 
-/**
- * @covers \GoodFirstIssue\GitHubAPIClient
- */
 final class GitHubAPIClientTest extends TestCase
 {
     public function test_instance_of_generator(): void
