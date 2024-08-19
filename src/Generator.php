a09156542ca970bb465c9cd3c67889c097fa961f
gomzyakov <alexander.gomzyakov@gmail.com>
2024-08-19 15:45:02 +0500

    Update Generator.php

--- src/Generator.php
+++ src/Generator.php
@@ -4,8 +4,7 @@ declare(strict_types = 1);
 
 namespace GoodFirstIssue;
 
-use GoodFirstIssue\DTO\Repository;
-use LogicException;
+use GuzzleHttp\Exception\GuzzleException;
 
 readonly class Generator
 {
@@ -15,45 +14,26 @@ readonly class Generator
     ) {
     }
 
+    /**
+     * @throws GuzzleException
+     *
+     * @return void
+     */
     public function run(): void
     {
         if (! is_dir($this->root_path . '/lang')) {
             mkdir($this->root_path . '/lang');
         }
 
-        // Get repository names from `repositories.json` file
-        $json = file_get_contents('repositories.json');
-        if (! is_string($json)) {
-            throw new LogicException('Cannot read file: repositories.json');
-        }
+        // TODO
+        //        foreach ($prog_lamguages as $prog_lamguage) {
+        $issues = $this->github_api_client->searchIssuesWithGoodFirtsIssueTag();
+        print_r('Issues count: ' . count($issues) . PHP_EOL);
+        //        }
 
-        $repository_names = json_decode($json, true);
-        if (! is_array($repository_names)) {
-            throw new LogicException('Cannot decode repository names');
-        }
 
-        print_r($repository_names);
-
-        $repositories = $this->github_api_client->requestRepositoriesData($repository_names);
-
-        // TODO сортируем репозитории по updated at
-        // TODO удаляем неактивные репозитории
-
-        foreach ($repositories as $repository) {
-            print_r('Get issues for repository: ' . $repository->full_name . "\n");
-
-            $issues = $this->github_api_client->requestIssues($repository->full_name);
-            $repository->setIssues($issues);
-        }
 
         $renderer = new Renderer($this->root_path);
-        $renderer->renderIndexPage($repositories);
-
-        $repositories_by_language = [];
-        foreach ($repositories as $repository) {
-            $repositories_by_language[$repository->language][] = $repository;
-        }
-
-        // TODO $renderer->renderPerLanguagePage($repositories);
+        $renderer->renderPage($issues, 'index', 'php'); // TODO
     }
 }
