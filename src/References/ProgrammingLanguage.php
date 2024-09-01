462acac4fd12869665a093b6db878581b2566837
gomzyakov <alexander.gomzyakov@gmail.com>
2024-09-01 13:08:44 +0500

    Render only PHP programming language

--- src/References/ProgrammingLanguage.php
+++ src/References/ProgrammingLanguage.php
@@ -6,16 +6,16 @@ namespace GoodFirstIssue\References;
 
 enum ProgrammingLanguage: string
 {
-    case PHP = 'PHP';
-    // case Python          = 'Python';
-    // case JavaScript      = 'JavaScript';
-    //    case Java            = 'Java';
-    //    case HTML            = 'HTML';
-    //    case JupyterNotebook = 'Jupyter Notebook';
-    //    case TypeScript      = 'TypeScript';
-    //    case CSharp          = 'C#';
-    //    case CPlusPlus       = 'C++';
-    //    case CSS             = 'CSS';
-    // case Go              = 'Go';
-    //    case Ruby            = 'Ruby';
+    case PHP             = 'PHP';
+    case Python          = 'Python';
+    case JavaScript      = 'JavaScript';
+    case Java            = 'Java';
+    case HTML            = 'HTML';
+    case JupyterNotebook = 'Jupyter Notebook';
+    case TypeScript      = 'TypeScript';
+    case CSharp          = 'C#';
+    case CPlusPlus       = 'C++';
+    case CSS             = 'CSS';
+    case Go              = 'Go';
+    case Ruby            = 'Ruby';
 }
